58ea4b2a2a51cdc8955c0281024fddaaf752a41a
Sun Apple <priomise@PromiseX.local>
2024-08-06 23:58:32 +0800

    chore: dark home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -13,18 +13,22 @@ import HeroPng from '@/assets/images/hero.png';
       <Header />
     </el-header>
     <el-main class="main">
-      <section class="pt-12 lg:pt-28 pb-12 sm:pb-16 lg:pb-40 bg-white">
+      <section
+        class="pt-12 lg:pt-28 pb-12 sm:pb-16 lg:pb-40 bg-white dark:bg-black"
+      >
         <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div
             class="grid max-w-lg grid-cols-1 mx-auto lg:max-w-full lg:items-center lg:grid-cols-2 gap-y-12 lg:gap-x-16"
           >
             <div class="text-center lg:text-left">
               <h1
-                class="text-5xl font-bold leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:leading-tight lg:text-6xl font-pj"
+                class="text-5xl font-bold leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:leading-tight lg:text-6xl font-pj dark:text-gray-100"
               >
                 Automate the work you do every day.
               </h1>
-              <p class="mt-2 text-gray-600 sm:mt-8 font-inter text-2xl">
+              <p
+                class="mt-2 text-gray-600 sm:mt-8 font-inter text-2xl dark:text-gray-400"
+              >
                 Integrate applications and automate workflows across your
                 organization.
               </p>
